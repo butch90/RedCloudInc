bdc71586fd29ddf11e1b7943d32b34acf44b39e5
Your Name <you@example.com>
2017-01-10 21:47:32 +0100

    frontpage changes

--- www/app.js
+++ www/app.js
@@ -29,7 +29,7 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $lo
 		controller: "profileCtrl"
 	})
 	.otherwise({
-		templateUrl: "views/error.html"
+		templateUrl: "views/home.html"
 	});
 
 	$locationProvider.html5Mode(true);
